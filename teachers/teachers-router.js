1b457aaaaacb242e44db597c80e8891d3ab024a4
Michael Hart <michael@mhart.dev>
2019-07-29 01:19:29 -0400

    Teacher login endpoint implemented

--- teachers/teachers-router.js
+++ teachers/teachers-router.js
@@ -21,6 +21,29 @@ router.post('/register', validateTeacher, (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Teachers.findBy({ username })
+    .first()
+    .then(teacher => {
+      if (teacher && bcrypt.compareSync(password, teacher.password)) {
+        // produce a token
+        const token = generateToken(teacher);
+
+        res.status(200).json({
+          message: `Login successful`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Error logging in' });
+    });
+});
+
 function generateToken(teacher) {
   const jwtPayload = {
     subject: teacher.id,
@@ -30,6 +53,7 @@ function generateToken(teacher) {
   const jwtOptions = {
     expiresIn: '12h',
   };
+  
   return jwt.sign(jwtPayload, secrets.jwtSecret, jwtOptions);
 }
 
